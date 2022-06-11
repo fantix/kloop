9bdf0d1834852b6c91fab15f808a597567084c38
Fantix King <fantix.king@gmail.com>
2022-06-11 17:54:11 -0400

    Update URL in the license text

--- resolver/src/lib.rs
+++ resolver/src/lib.rs
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2022 Fantix King  http://fantix.pro
+Copyright (c) 2022 Fantix King  https://fantix.pro
 kLoop is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
